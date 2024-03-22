e4234b993e2a5a165cf2e0ea4602f357baa304c7
Munsi Walid Al Hassan Nizhu <76394555+walid3271@users.noreply.github.com>
2024-03-22 14:18:31 +0600

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-Model = YOLO('FastAPI-Face-Detection-using-pre-trained-yolov8-model/AI_Model/facev8.pt')
+Model = YOLO('FastAPI-Object-Detection-using-pre-trained-yolov8-model/AI_Model/facev8.pt')
 
 # import torch
 # Model = torch.hub.load('./yolov5', 'custom', path='./AI_Model/yolov5.pt', source='local')
